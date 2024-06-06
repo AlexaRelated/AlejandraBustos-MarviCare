07d5c3ca35b45d182b5256d46bd99af57d02acd8
Alejandra Bustos <mercedes_ale87@hotmail.com>
2024-06-06 23:37:03 +0200

    creando la base de datos

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for blog project.
+URL configuration for proyecto project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,26 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
-# Use include() to add paths from the catalog application
-
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
-]
-
-#Add URL maps to redirect the base URL to our application
-from django.views.generic import RedirectView
-urlpatterns += [
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
-]
-
-# Use static() to add url mapping to serve static files during development (only)
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
