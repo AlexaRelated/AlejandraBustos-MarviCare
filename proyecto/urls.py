272627f1da12dfd3440b012791da5641d8eeea06
Alejandra Bustos <mercedes_ale87@hotmail.com>
2024-06-07 19:12:25 +0200

    cambio de nombre carpeta cuentas a inicio

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from cuentas.views import inicio, template1
+from inicio.views import inicio, template1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
