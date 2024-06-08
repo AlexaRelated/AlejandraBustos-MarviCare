4e24754220a7d16848c65be25d29d1e7b4b6e242
Alejandra Bustos <mercedes_ale87@hotmail.com>
2024-06-08 11:43:31 +0200

    actualizando archivos de la carpeta inicio

--- inicio/views.py
+++ inicio/views.py
@@ -5,6 +5,3 @@ from django.http import HttpResponse
 def inicio(request):
     return HttpResponse ('Bienvenidos a mi inicio')
 
-def template1(request):
-    fecha = datetime.now()
-    return HttpResponse(f'<h1> Mi Template 1</h1> -- Fecha: {fecha}')
