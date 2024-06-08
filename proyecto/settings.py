e6c969178e91797579187f4cc2fc9bc8da853c46
Alejandra Bustos <mercedes_ale87@hotmail.com>
2024-06-08 19:57:19 +0200

    actualizando plantillas

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -130,7 +130,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'inicio', 'static'),
+    os.path.join(BASE_DIR, 'inicio/static'),
 ]
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
