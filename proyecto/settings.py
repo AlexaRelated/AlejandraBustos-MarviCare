2e49b9e3ea9a7eb82942ccdd47d3c8299c8c48e0
Alejandra Bustos <mercedes_ale87@hotmail.com>
2024-06-10 12:48:11 +0200

    actualizar settings

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -29,8 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-sys.path.append(os.path.join(BASE_DIR, 'myenv', 'blog'))
-
 # Application definition
 
 BLOG_NAME = 'MarviCare'
@@ -125,27 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'inicio/static'),
-]
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    os.path.join(BASE_DIR, 'inicio', 'static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static/')
-#    ]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
