2f2b07db15e421ea6c432e0e2f52a76221f0ce15
Alejandra Bustos <mercedes_ale87@hotmail.com>
2024-06-18 18:27:38 +0200

    reparando error en categorias de post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 # blog/views.py
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Post, Author, Article, Category, BlogPost, Comment
 from .forms import CommentForm  # Importar desde forms.py
 from django.db.models import Q
@@ -50,12 +50,16 @@ def search_posts(request):
 def post_detail(request, pk):
     post = get_object_or_404(BlogPost, pk=pk)
     comments = post.comments.all()
+
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
+            # Después de guardar el comentario, podrías redirigir a la misma página o a otra página
+            return redirect('post_detail', pk=pk)  # Reemplaza 'post_detail' con el nombre de tu vista de detalle de post
     else:
         form = CommentForm()
+
     return render(request, 'post_detail.html', {'post': post, 'comments': comments, 'form': form})
