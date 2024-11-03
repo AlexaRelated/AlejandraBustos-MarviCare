91f8db262d989f53c281ed375e2c3c93fcb121e7
Alejandra Bustos <mercedes_ale87@hotmail.com>
2024-11-03 17:33:17 +0100

    cambio fichero logo

--- usuarios/models.py
+++ usuarios/models.py
@@ -20,3 +20,14 @@ class Author(models.Model):
     def __str__(self):
         return self.user.username
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
